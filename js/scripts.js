cf184ebbb3725eafa2e37474b9ff18e22f3a8c04
saayeeem <chysayem9@gmail.com>
2021-05-10 02:38:52 +0600

    npm scripts 2

--- js/scripts.js
+++ js/scripts.js
@@ -29,3 +29,19 @@ $(document).ready(function(){
   });
 });
             
+            
+        // $(document).ready(function(){
+        //     $('[data-toggle="tooltip"]').tooltip();
+        // });
+  
+  
+        // $(document).ready(function(){
+        //     $("#mycarousel").carousel( { interval: 2000 } );
+        //     $("#carousel-pause").click(function(){
+        //         $("#mycarousel").carousel('pause');
+        //     });
+        //     $("#carousel-play").click(function(){
+        //         $("#mycarousel").carousel('cycle');
+        //     });
+        // });
+   
